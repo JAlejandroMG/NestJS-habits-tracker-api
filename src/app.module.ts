4548f1586270cb594e746c8cadf303b4b6c00bf1
JAlejandroMG <jalejandromg@tutanota.com>
2025-09-05 15:27:29 -0600

    feat(feature): ✨ 01-NewModule
    Add new module with controller and service files, and add it to AppModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HabitsModule } from './habits/habits.module';
 
 @Module({
-  imports: [],
+  imports: [HabitsModule], //* Added in 01-NewModule branch/commit with habits folder
   controllers: [AppController],
   providers: [AppService],
 })
