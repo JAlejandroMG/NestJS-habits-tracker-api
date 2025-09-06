546cc9899bde707debd57cee9b2e5edfddde924c
JAlejandroMG <jalejandromg@tutanota.com>
2025-09-05 21:54:07 -0600

    Last commit cleanup

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { HabitsModule } from './habits/habits.module';
 
 @Module({
-  imports: [HabitsModule], //* Added in 01-NewModule branch/commit with habits folder
+  imports: [HabitsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/habits/habits.controller.ts
+++ src/habits/habits.controller.ts
@@ -1,17 +1,12 @@
-import { Controller, Get /*, Inject*/ } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { HabitsService } from './habits.service';
 
 @Controller('habits')
 export class HabitsController {
   constructor(private readonly habitsService: HabitsService) {}
-  //~ @Inject(HabitsService)
-  //~ private readonly habitService: HabitsService;
 
   @Get()
   findAll() {
-    //~1 const habitsService = new HabitsService();
-
-    //~1 return habitsService.findAll();
     return this.habitsService.findAll();
   }
 }
