63436e348157b02515fce9b2eeeefb8ad54ed4dd
JAlejandroMG <jalejandromg@tutanota.com>
2025-09-20 17:59:32 -0600

    Last commit cleanup

--- src/app-config/app-config.module.ts
+++ src/app-config/app-config.module.ts
@@ -4,12 +4,9 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   exports: [AppConfigService],
-  //* Centralize all configuration in this module
-  //* validations here are spot before runtime
   imports: [
     ConfigModule.forRoot({
       validate: (config: Record<string, any>) => {
-        //~ Validation for showing data limit
         if (!config.DEFAULT_LIMIT) {
           config.DEFAULT_LIMIT = 10;
         } else {
@@ -22,15 +19,12 @@ import { ConfigModule } from '@nestjs/config';
           config.DEFAULT_LIMIT = limit;
         }
 
-        //~ Validation for seed data
         if (!config.SEED_DATA_FILE_PATH) {
           config.SEED_DATA_FILE_PATH = 'fixtures/seed-data.json';
         } else if (!config.SEED_DATA_FILE_PATH.endsWith('.json')) {
           throw new Error('Invalid SEED_DATA_FILE_PATH');
         }
 
-        //* Validation here can set the config values before returning it
-        //- No longer needed at Service Level
         return config;
       },
     }),
--- src/app-config/app-config.service.ts
+++ src/app-config/app-config.service.ts
@@ -5,41 +5,11 @@ import { ConfigService } from '@nestjs/config';
 export class AppConfigService {
   constructor(private readonly configService: ConfigService) {}
 
-  //* Validation first approach directly here
-  //* validations here are spot at runtime
-
   get defaultLimit(): number {
-    //- All this is no longer needed if
-    //- validation is made at Module Level
-    /* const limit: number = parseInt(
-      this.configService.get('DEFAULT_LIMIT', '10'),
-    );
-
-    //* Added
-    if (isNaN(limit) || limit <= 0) {
-      throw new Error('Invalid DEFAULT_LIMIT');
-    }
-
-    return limit; */
-
     return this.configService.get<number>('DEFAULT_LIMIT')!;
   }
 
   get seedDataFilePath(): string {
-    //- All this is no longer needed if
-    //- validation is made at Module Level
-    /*const filePath: string = this.configService.get(
-      'SEED_DATA_FILE_PATH',
-      'fixtures/seed-data.json',
-    );
-
-    //* Added
-    if (!filePath || !filePath.endsWith('.json')) {
-      throw new Error('Invalid SEED_DATA_FILE_PATH');
-    }
-
-    return filePath; */
-
     return this.configService.get<string>('SEED_DATA_FILE_PATH')!;
   }
 }
