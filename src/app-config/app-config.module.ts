a1b71d95987bffc7bf72fb8b30ba33aee32bc4ad
JAlejandroMG <jalejandromg@tutanota.com>
2025-09-20 17:56:12 -0600

    feat(feature): ✨ 21-ConfigValidation
    Config validation at service and module levels.

--- src/app-config/app-config.module.ts
+++ src/app-config/app-config.module.ts
@@ -5,7 +5,36 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   exports: [AppConfigService],
   //* Centralize all configuration in this module
-  imports: [ConfigModule.forRoot()],
+  //* validations here are spot before runtime
+  imports: [
+    ConfigModule.forRoot({
+      validate: (config: Record<string, any>) => {
+        //~ Validation for showing data limit
+        if (!config.DEFAULT_LIMIT) {
+          config.DEFAULT_LIMIT = 10;
+        } else {
+          const limit = parseInt(config.DEFAULT_LIMIT);
+
+          if (isNaN(limit) || limit <= 0) {
+            throw new Error('Invalid DEFAULT_LIMIT');
+          }
+
+          config.DEFAULT_LIMIT = limit;
+        }
+
+        //~ Validation for seed data
+        if (!config.SEED_DATA_FILE_PATH) {
+          config.SEED_DATA_FILE_PATH = 'fixtures/seed-data.json';
+        } else if (!config.SEED_DATA_FILE_PATH.endsWith('.json')) {
+          throw new Error('Invalid SEED_DATA_FILE_PATH');
+        }
+
+        //* Validation here can set the config values before returning it
+        //- No longer needed at Service Level
+        return config;
+      },
+    }),
+  ],
   providers: [AppConfigService],
 })
 export class AppConfigModule {}
