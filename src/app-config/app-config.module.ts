9bfd08fc0c3a88def68f187c16f2ebdefd5ea759
JAlejandroMG <jalejandromg@tutanota.com>
2025-09-20 19:29:01 -0600

    Last commit cleanup

--- src/app-config/app-config.module.ts
+++ src/app-config/app-config.module.ts
@@ -10,35 +10,6 @@ import { AppConfigService } from './app-config.service';
   exports: [AppConfigService],
   imports: [
     ConfigModule.forRoot({
-      //   validate: (config: Record<string, any>) => {
-      /*if (!config.DEFAULT_LIMIT) {
-          config.DEFAULT_LIMIT = 10;
-        } else {
-          const limit = parseInt(config.DEFAULT_LIMIT);
-
-          if (isNaN(limit) || limit <= 0) {
-            throw new Error('Invalid DEFAULT_LIMIT');
-          }
-
-          config.DEFAULT_LIMIT = limit;
-        }*/
-
-      //*Added Zod implementation
-      /*const ConfigSchema = z.object({
-          DEFAULT_LIMIT: z.coerce
-            .number()
-            .int()
-            .positive()
-            .optional()
-            .default(10),
-          SEED_DATA_FILE_PATH: z
-            .string()
-            .endsWith('.json')
-            .optional()
-            .default('fixtures/seed-data.json'),
-        });*/
-
-      //* Add Joi imolementation
       validationSchema: Joi.object({
         DEFAULT_LIMIT: Joi.number().optional().integer().positive().default(10),
         SEED_DATA_FILE_PATH: Joi.string()
@@ -46,17 +17,6 @@ import { AppConfigService } from './app-config.service';
           .default('fixtures/seed-data.json')
           .regex(/\.json$/),
       }),
-
-      /*if (!config.SEED_DATA_FILE_PATH) {
-          config.SEED_DATA_FILE_PATH = 'fixtures/seed-data.json';
-        } else if (!config.SEED_DATA_FILE_PATH.endsWith('.json')) {
-          throw new Error('Invalid SEED_DATA_FILE_PATH');
-        }*/
-
-      // return config;
-      //* Add Zod implementation
-      // return ConfigSchema.parse(config);
-      //   },
     }),
   ],
 
