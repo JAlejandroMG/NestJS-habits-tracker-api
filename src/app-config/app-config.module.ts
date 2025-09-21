ab4cf444adbdadcd0134d5cb3943784d4fc56a8b
JAlejandroMG <jalejandromg@tutanota.com>
2025-09-20 19:27:24 -0600

    feat(feature): ✨ 22-ExternalLibraryValidation
    Add and implement Joi and Zod libraries for validation.

--- src/app-config/app-config.module.ts
+++ src/app-config/app-config.module.ts
@@ -1,13 +1,17 @@
+import * as Joi from 'joi';
+// import z from 'zod';
+
 import { Module } from '@nestjs/common';
-import { AppConfigService } from './app-config.service';
 import { ConfigModule } from '@nestjs/config';
+
+import { AppConfigService } from './app-config.service';
 
 @Module({
   exports: [AppConfigService],
   imports: [
     ConfigModule.forRoot({
-      validate: (config: Record<string, any>) => {
-        if (!config.DEFAULT_LIMIT) {
+      //   validate: (config: Record<string, any>) => {
+      /*if (!config.DEFAULT_LIMIT) {
           config.DEFAULT_LIMIT = 10;
         } else {
           const limit = parseInt(config.DEFAULT_LIMIT);
@@ -17,18 +21,45 @@ import { ConfigModule } from '@nestjs/config';
           }
 
           config.DEFAULT_LIMIT = limit;
-        }
+        }*/
 
-        if (!config.SEED_DATA_FILE_PATH) {
+      //*Added Zod implementation
+      /*const ConfigSchema = z.object({
+          DEFAULT_LIMIT: z.coerce
+            .number()
+            .int()
+            .positive()
+            .optional()
+            .default(10),
+          SEED_DATA_FILE_PATH: z
+            .string()
+            .endsWith('.json')
+            .optional()
+            .default('fixtures/seed-data.json'),
+        });*/
+
+      //* Add Joi imolementation
+      validationSchema: Joi.object({
+        DEFAULT_LIMIT: Joi.number().optional().integer().positive().default(10),
+        SEED_DATA_FILE_PATH: Joi.string()
+          .optional()
+          .default('fixtures/seed-data.json')
+          .regex(/\.json$/),
+      }),
+
+      /*if (!config.SEED_DATA_FILE_PATH) {
           config.SEED_DATA_FILE_PATH = 'fixtures/seed-data.json';
         } else if (!config.SEED_DATA_FILE_PATH.endsWith('.json')) {
           throw new Error('Invalid SEED_DATA_FILE_PATH');
-        }
+        }*/
 
-        return config;
-      },
+      // return config;
+      //* Add Zod implementation
+      // return ConfigSchema.parse(config);
+      //   },
     }),
   ],
+
   providers: [AppConfigService],
 })
 export class AppConfigModule {}
