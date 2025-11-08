ea5bae214e035849a9032acbcb6b5a7dbd5e9a56
JAlejandroMG <jalejandromg@tutanota.com>
2025-11-08 16:13:06 -0600

    feat(feature): ✨ 59-CustomMiddlewarePartially
    The custom middleware has been applied to the habits module.

--- src/habits/habits.module.ts
+++ src/habits/habits.module.ts
@@ -1,17 +1,37 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import {
+  DynamicModule,
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 
 import { HabitsController } from './controllers/habits.controller';
 import { HabitsService } from './services/habits.service';
 import { AppConfigModule } from 'src/app-config/app-config.module';
 import { HabitsRepositoryModule } from './repositories/habits-repository.module';
-import { DbType } from 'src/utils/constants';
+import { DbType, HABITS } from 'src/utils/constants';
+import { catchMaliciousInput } from 'src/utils/middleware/catch-malicious-input.middleware';
 
 @Module({
   controllers: [HabitsController],
   imports: [AppConfigModule],
   providers: [HabitsService],
 })
-export class HabitsModule {
+export class HabitsModule implements NestModule {
+  //* Added
+  configure(consumer: MiddlewareConsumer) {
+    //~ Indicates the middleware is for endpoints startind with HABITS
+    // consumer.apply(catchMaliciousInput).forRoutes(HABITS);
+    //~ Indicates the middleware only applies for the root defined
+    //~ inside the HabitsController
+    consumer
+      .apply(catchMaliciousInput)
+      //~ For GET request in the habits endpoint, middleware will be ignored
+      .exclude({ path: HABITS, method: RequestMethod.GET })
+      .forRoutes(HabitsController);
+  }
+
   static register(options: { dbType: DbType }): DynamicModule {
     return {
       imports: [HabitsRepositoryModule.register({ dbType: options.dbType })],
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import * as cors from 'cors';
 import helmet from 'helmet';
-import { json } from 'express';
+// import { json } from 'express';
 
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 import { DbType } from './utils/constants';
-import { catchMaliciousInput } from './utils/middleware/catch-malicious-input.middleware';
+// import { catchMaliciousInput } from './utils/middleware/catch-malicious-input.middleware';
 
 async function bootstrap() {
   const appDataDb = (process.env.APP_DATA_DB as DbType) ?? DbType.IN_MEMORY;
@@ -26,8 +26,14 @@ async function bootstrap() {
   //~ there is no body property in the Request object
   //~ because the data is received in form of a stream
   //~ that needs to be converted into a json object
+  //* But when middleware is consumed inside a NestModule,
+  //* NestJS already run some default middleware first
+  //* such as parsing the body to JSON, or URL parameters
+  //* so therefore calling json() is no longer required here
+  //   app.use(cors(), helmet(), json(), catchMaliciousInput);
+  //* Modified to apply only to habits controller
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-  app.use(cors(), helmet(), json(), catchMaliciousInput);
+  app.use(cors(), helmet());
 
   app.useGlobalPipes(
     new ValidationPipe({
