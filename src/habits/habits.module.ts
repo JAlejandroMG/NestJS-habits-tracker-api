3f1046283dc91e5c57520abe53dc20a6d2aef38f
JAlejandroMG <jalejandromg@tutanota.com>
2025-11-08 20:57:12 -0600

    feat(feature): ✨ 60-DependenciesMiddlewareClass
    Modified the custom Middleware for a Class to inject other services directly.

--- src/habits/habits.module.ts
+++ src/habits/habits.module.ts
@@ -11,7 +11,10 @@ import { HabitsService } from './services/habits.service';
 import { AppConfigModule } from 'src/app-config/app-config.module';
 import { HabitsRepositoryModule } from './repositories/habits-repository.module';
 import { DbType, HABITS } from 'src/utils/constants';
-import { catchMaliciousInput } from 'src/utils/middleware/catch-malicious-input.middleware';
+import {
+  CatchMaliciousInput,
+  //   catchMaliciousInput,
+} from 'src/utils/middleware/catch-malicious-input.middleware';
 import { AppConfigService } from 'src/app-config/app-config.service';
 
 @Module({
@@ -28,8 +31,14 @@ export class HabitsModule implements NestModule {
     //~ Indicates the middleware only applies for the root defined
     //~ inside the HabitsController
     consumer
+      //* Modified
       // To pass param to the factory function
-      .apply(catchMaliciousInput(this.appConfigService.maxBodySize))
+      //   .apply(catchMaliciousInput(this.appConfigService.maxBodySize))
+      //* Now we're passing a middleware as a Class
+      //~ Since the middleware Class has a injected dependency
+      //~ we need to make sure we have the same dependency available here
+      //~ which is the case for an AppConfigService through AppConfigModule
+      .apply(CatchMaliciousInput)
       //~ For GET request in the habits endpoint, middleware will be ignored
       .exclude({ path: HABITS, method: RequestMethod.GET })
       .forRoutes(HabitsController);
--- src/utils/middleware/catch-malicious-input.middleware.ts
+++ src/utils/middleware/catch-malicious-input.middleware.ts
@@ -1,23 +1,33 @@
 import { NextFunction, Request, Response } from 'express';
 import { hasExceededMaxSize } from './has-exceeded-max-size';
-import { HttpStatus } from '@nestjs/common';
+import { HttpStatus, Injectable, NestMiddleware } from '@nestjs/common';
 import { hasSuspiciousPatterns } from './has-suspicious-patterns';
+import { AppConfigService } from 'src/app-config/app-config.service';
 
-export const catchMaliciousInput =
-  (maxBodySize: number) =>
-  (
+//* Modified
+// export const catchMaliciousInput =
+//   (maxBodySize: number) =>
+//~ To make sure this class will be managed by NestJS
+@Injectable()
+export class CatchMaliciousInput implements NestMiddleware {
+  constructor(private readonly appConfigService: AppConfigService) {}
+
+  use(
     req: Request,
     //~ With this Response object can be terminated the Request cycle
     //~ by using the Reponse object, and sending the response to the API client
     //~ so the control doesn't have to be released over the next middleware in the chain
     res: Response,
     next: NextFunction,
-  ) => {
+    //*Modified
+    //   ) => {
+  ) {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const { body } = req;
 
-    if (hasExceededMaxSize(body, maxBodySize)) {
-      console.log('Max body size', maxBodySize);
+    //* Modified
+    // if (hasExceededMaxSize(body, maxBodySize)) {
+    if (hasExceededMaxSize(body, this.appConfigService.maxBodySize)) {
       return res.status(HttpStatus.BAD_REQUEST).send({
         message: 'Request body exceeds size limit',
         statusCode: HttpStatus.BAD_REQUEST,
@@ -36,4 +46,5 @@ export const catchMaliciousInput =
     //~ If this is not called the Request will just hang
     //~ and the API client will never receive a Response
     next();
-  };
+  }
+}
