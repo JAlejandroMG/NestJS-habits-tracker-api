3faf18ac5f4e47e48f33f7069cbcc57f11503f7c
JAlejandroMG <jalejandromg@tutanota.com>
2026-01-03 19:08:33 -0600

    test(test): ✅ 63-TestingClasses
    Add Class test handled by nestjs testing, so doing it closer to reality.

--- src/utils/middleware/catch-malicious-input.middleware.spec.ts
+++ src/utils/middleware/catch-malicious-input.middleware.spec.ts
@@ -1,5 +1,77 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  createRequest,
+  createResponse,
+  MockRequest,
+  MockResponse,
+} from 'node-mocks-http';
+import { HttpStatus } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+
+import { AppConfigModule } from 'src/app-config/app-config.module';
+import { AppConfigService } from 'src/app-config/app-config.service';
+import { CatchMaliciousInput } from './catch-malicious-input.middleware';
+
 describe('CatchMaliciousInput', () => {
+  let appConfigService: AppConfigService;
+  let middleware: CatchMaliciousInput;
+
+  beforeEach(async () => {
+    //~ In order to get a valid module reference needs to call compile fn
+    const moduleRef = await Test.createTestingModule({
+      //~ By doing this NestJS will load all depencencies for AppConfigService
+      imports: [AppConfigModule],
+      providers: [CatchMaliciousInput],
+    }).compile();
+
+    appConfigService = moduleRef.get<AppConfigService>(AppConfigService);
+    middleware = moduleRef.get<CatchMaliciousInput>(CatchMaliciousInput);
+
+    jest.resetAllMocks();
+  });
+
+  it('should be defined', () => {
+    expect(middleware).toBeDefined();
+  });
+
   describe('use', () => {
-    it('should return "BAD_REQUEST" if body is too large', () => {});
+    describe('when body is too large', () => {
+      let response: MockResponse<any>;
+      let request: MockRequest<any>;
+      let next: jest.Mock;
+
+      beforeEach(() => {
+        //~ Arrange
+        jest.spyOn(appConfigService, 'maxBodySize', 'get').mockReturnValue(10);
+
+        request = createRequest({
+          body: { description: 'a', name: 'test' },
+          method: 'POST',
+        });
+        response = createResponse();
+        next = jest.fn();
+
+        //~ Act
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        middleware.use(request, response, next);
+      });
+
+      it('should set response status code to "BAD_REQUEST"', () => {
+        expect(response.statusCode).toBe(HttpStatus.BAD_REQUEST);
+      });
+
+      it('should set response data object', () => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+        expect(response._getData()).toEqual({
+          message: 'Request body exceeds size limit',
+          statusCode: HttpStatus.BAD_REQUEST,
+        });
+      });
+
+      it('should not call the next middleware', () => {
+        expect(next).not.toHaveBeenCalled();
+      });
+    });
   });
 });
